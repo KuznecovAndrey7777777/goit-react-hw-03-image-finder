5aaec6c86d9837a1b15a54f3c3fb19e56e5fc22c
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2024-01-08 20:56:15 +0200

    save0801242056

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
+import fetchImages from './api';
 import './App.css';
 
 class App extends Component {
@@ -18,36 +19,33 @@ class App extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery || prevState.currentPage !== this.state.currentPage) {
-      this.fetchImages();
+    if (
+      prevState.searchQuery !== this.state.searchQuery ||
+      prevState.currentPage !== this.state.currentPage
+    ) {
+      this.loadImages();
     }
   }
 
-  fetchImages = async () => {
-    const { searchQuery, currentPage } = this.state;
-    const apiKey = '31731640-63415b264c7abe0734c9208e1';
-    const url = `https://pixabay.com/api/?q=${encodeURIComponent(searchQuery)}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`;
-
+  loadImages = async () => {
     this.setState({ isLoading: true });
 
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Помилка отримання зображень');
-      }
-      const data = await response.json();
+      const { images, totalHits } = await fetchImages(
+        this.state.searchQuery,
+        this.state.currentPage
+      );
       this.setState(prevState => ({
-        images: [...prevState.images, ...data.hits],
+        images: [...prevState.images, ...images],
         isLoading: false,
-        hasMoreImages: data.hits.length > 0,
+        hasMoreImages: this.state.currentPage < Math.ceil(totalHits / 12),
       }));
     } catch (error) {
-      console.error('Під час отримання зображень сталася помилка:', error);
-      this.setState({ isLoading: false, hasMoreImages: false });
+      this.setState({ isLoading: false });
     }
   };
 
-  handleSearchSubmit = (query) => {
+  handleSearchSubmit = query => {
     this.setState({
       searchQuery: query,
       currentPage: 1,
@@ -62,7 +60,7 @@ class App extends Component {
     }));
   };
 
-  openModal = (imageURL) => {
+  openModal = imageURL => {
     this.setState({
       showModal: true,
       largeImageURL: imageURL,
@@ -77,7 +75,8 @@ class App extends Component {
   };
 
   render() {
-    const { images, isLoading, showModal, largeImageURL, hasMoreImages } = this.state;
+    const { images, isLoading, showModal, largeImageURL, hasMoreImages } =
+      this.state;
 
     return (
       <div className="App">
@@ -87,12 +86,105 @@ class App extends Component {
         {images.length > 0 && hasMoreImages && !isLoading && (
           <Button onLoadMore={this.handleLoadMore} />
         )}
-        {showModal && (
-          <Modal image={largeImageURL} onClose={this.closeModal} />
-        )}
+        {showModal && <Modal image={largeImageURL} onClose={this.closeModal} />}
       </div>
     );
   }
 }
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     images: [],
+//     currentPage: 1,
+//     searchQuery: '',
+//     isLoading: false,
+//     showModal: false,
+//     largeImageURL: '',
+//     hasMoreImages: true,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.searchQuery !== this.state.searchQuery ||
+//       prevState.currentPage !== this.state.currentPage
+//     ) {
+//       this.fetchImages();
+//     }
+//   }
+
+//   fetchImages = async () => {
+//     const { searchQuery, currentPage } = this.state;
+//     const apiKey = '31731640-63415b264c7abe0734c9208e1';
+//     const url = `https://pixabay.com/api/?q=${encodeURIComponent(
+//       searchQuery
+//     )}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`;
+
+//     this.setState({ isLoading: true });
+
+//     try {
+//       const response = await fetch(url);
+//       if (!response.ok) {
+//         throw new Error('Помилка отримання зображень');
+//       }
+//       const data = await response.json();
+//       this.setState(prevState => ({
+//         images: [...prevState.images, ...data.hits],
+//         isLoading: false,
+//         hasMoreImages: data.hits.length > 0,
+//       }));
+//     } catch (error) {
+//       console.error('Під час отримання зображень сталася помилка:', error);
+//       this.setState({ isLoading: false, hasMoreImages: false });
+//     }
+//   };
+
+//   handleSearchSubmit = query => {
+//     this.setState({
+//       searchQuery: query,
+//       currentPage: 1,
+//       images: [],
+//       hasMoreImages: true,
+//     });
+//   };
+
+//   handleLoadMore = () => {
+//     this.setState(prevState => ({
+//       currentPage: prevState.currentPage + 1,
+//     }));
+//   };
+
+//   openModal = imageURL => {
+//     this.setState({
+//       showModal: true,
+//       largeImageURL: imageURL,
+//     });
+//   };
+
+//   closeModal = () => {
+//     this.setState({
+//       showModal: false,
+//       largeImageURL: '',
+//     });
+//   };
+
+//   render() {
+//     const { images, isLoading, showModal, largeImageURL, hasMoreImages } =
+//       this.state;
+
+//     return (
+//       <div className="App">
+//         <Searchbar onSubmit={this.handleSearchSubmit} />
+//         <ImageGallery images={images} onImageClick={this.openModal} />
+//         {isLoading && <Loader />}
+//         {images.length > 0 && hasMoreImages && !isLoading && (
+//           <Button onLoadMore={this.handleLoadMore} />
+//         )}
+//         {showModal && <Modal image={largeImageURL} onClose={this.closeModal} />}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
